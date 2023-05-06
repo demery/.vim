2dfb015ecdab0c45336c69ae1f899ddc2a714e83
Doug Emery <doug.emery@gmail.com>
2023-05-06 10:05:02 -0400

    Add messages, plugin lists to setup; update plugins
    Remove youcompleteme plugin; add vim-better-whitespace

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,15 +1,55 @@
 #!/usr/bin/env bash
 
 this_dir=$(dirname $0)
-(
-cd ${this_dir}/bundle
+cmd=$(basename $0)
 
-git clone https://github.com/godlygeek/tabular
-git clone https://github.com/preservim/nerdtree.git 
-git clone https://github.com/scrooloose/nerdcommenter
-git clone https://github.com/tpope/vim-endwise
-git clone https://github.com/tpope/vim-fugitive
-git clone https://github.com/tpope/vim-surround
-git clone https://github.com/valloric/youcompleteme
+# Create the autoload and pathogen directories if they don't exist
+for subdir in autoload bundle
+do
+  echo "[${cmd}] Ensure directory exists: '${subdir}'"
+  [[ -e ${this_dir}/${subdir} ]] || mkdir ${this_dir}/${subdir}
+done
+
+# install pathogen
+echo "[${cmd}] Make sure pathogen is installed"
+curl -LSso ${this_dir}/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+read -r -d '' PLUGINS <<'EOF'
+  https://github.com/godlygeek/tabular
+  https://github.com/ntpeters/vim-better-whitespace
+  https://github.com/preservim/nerdtree.git
+  https://github.com/scrooloose/nerdcommenter
+  https://github.com/tpope/vim-endwise
+  https://github.com/tpope/vim-fugitive
+  https://github.com/tpope/vim-surround
+EOF
+
+read -r -d '' COLORSCHEMES <<'EOF'
+  https://github.com/tpope/vim-vividchalk
+  https://github.com/wesgibbs/vim-irblack
+EOF
+
+# add these bundles
+(
+  cd ${this_dir}/bundle
+
+  for plugin in ${PLUGINS}
+  do
+    echo "[${cmd}] Ensure plugin installed '${plugin}'"
+    git clone ${plugin}
+  done
+
+  # colorschemes
+  for colorscheme in ${COLORSCHEMES}
+  do
+    echo "[${cmd}] Ensure colorscheme installed '${colorscheme}'"
+    git clone ${colorscheme}
+  done
 
 )
+
+
+# Create the vim temp dir
+vimtmp=${HOME}/.vim-tmp
+echo "[${cmd}] Ensure temp dir exists '${vimtmp}'"
+[[ -e ${vimtmp} ]] || mkdir -v ${vimtmp}
